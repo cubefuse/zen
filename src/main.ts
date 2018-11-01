6d4d9ccab5a1caa29d8a7d05348e2fa7f00a81ac
Madusha Prasanjith <mprasanjith@gmail.com>
2018-11-02 00:32:31 +0530

    Remove Shards.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import Vue from "vue";
-// @ts-ignore
-import ShardsVue from "shards-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-// Import base styles (Bootstrap and Shards)
-import "bootstrap/dist/css/bootstrap.css";
-import "shards-ui/dist/css/shards.css";
-
-Vue.use(ShardsVue);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="home">
-    <d-card style="max-width: 300px">
-      <d-card-header>Card header</d-card-header>
-      <d-card-img src="https://place-hold.it/300x200"/>
-      <d-card-body title="Lorem Ipsum">
-        <p>Lorem ipsum dolor sit amet.</p>
-        <d-btn theme="primary">Read more &rarr;</d-btn>
-      </d-card-body>
-      <d-card-footer>Card footer</d-card-footer>
-    </d-card>
-  </div>
+  <h1>Test</h1>
 </template>
 
 <script lang="ts">
