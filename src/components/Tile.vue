91dbb1279380ea737f694aa239b0b0fdee10c753
Madusha Prasanjith <mprasanjith@gmail.com>
2018-11-03 11:40:02 +0530

    Add Tile grid UI

---
+++ src/components/Tile.vue
@@ -0,0 +1,34 @@
+<template>
+    <router-link to="/token">
+        <div class="tile is-parent">
+            <article :class="['tile is-child notification is-' + color ]">
+                <div class="level is-mobile">
+                    <div class="level-item level-left">
+                        <div class="image is-24x24">
+                            <img :src="iconUrl" />
+                        </div>
+                    </div>
+                    <div class="level-item has-text-centered level-left">
+                        <p class="subtitle">{{name}}</p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Tile extends Vue {
+  @Prop()
+  name!: string;
+
+  @Prop()
+  iconUrl!: string;
+
+  @Prop({ default: "light" })
+  color!: string;
+}
+</script>
---
+++ src/views/TileGrid.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="hero">
+        <div class="hero-head">
+            <nav-bar />
+        </div>
+        <section class="section">
+            <div class="tile is-ancestor">
+                <Tile name="Facebook" iconUrl="https://static.xx.fbcdn.net/rsrc.php/yo/r/iRmz9lCMBD2.ico" color="dark" />
+                <Tile name="Github" iconUrl="https://github.com/favicon.ico" color="light" />
+                <Tile name="Github" iconUrl="https://github.com/favicon.ico" color="light" />
+                <Tile name="Github" iconUrl="https://github.com/favicon.ico" color="light" />
+                <Tile name="Github" iconUrl="https://github.com/favicon.ico" color="light" />
+                <Tile name="Github" iconUrl="https://github.com/favicon.ico" color="light" />
+            </div>
+        </section>
+    </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Tile from "../components/Tile.vue";
+import NavBar from "../components/NavBar.vue";
+
+@Component({
+  components: { NavBar, Tile }
+})
+export default class TileGrid extends Vue {}
+</script>
