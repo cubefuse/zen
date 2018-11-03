2454b692efd01e927aa219d11968ebbfbd2f963f
Madusha Prasanjith <mprasanjith@gmail.com>
2018-11-03 11:37:46 +0530

    Add Buefy and Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import Vue from "vue";
+import Buefy from "buefy";
+// @ts-ignore
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "buefy/dist/buefy.css";
+
+Vue.component("fas", FontAwesomeIcon);
+
+Vue.use(Buefy, { defaultIconPack: "fas" });
 
 Vue.config.productionTip = false;
 
